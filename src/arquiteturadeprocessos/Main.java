8ed8f566d8913dfb807c44e6b05dd479d92151ef
John Haste <joaovictorspteixeira@gmail.com>
2018-04-07 02:34:04 -0300

    Todo:atualizar a reputação do usuário após falha no envio. Feito: transferência de arquivos em txt nas pastas locais corretas

--- src/arquiteturadeprocessos/Main.java
+++ src/arquiteturadeprocessos/Main.java
@@ -6,16 +6,45 @@
 package arquiteturadeprocessos;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author John Haste
  */
 public class Main {
-    
-    public static void main(String[] args) {
         
+    public static void main(String[] args) {
+     
+        //Pré definição para debug
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Selecione um usuário");
+        System.out.println("1-Lula\n2-Aecio\n3-Dilma");
+        switch(teclado.nextInt()){
+            case 1:
+                ProcessoUsuario a = new ProcessoUsuario("Lula",3333);
+                a.SendOlah("MULTICAST",3333);
+                iniciaConexao(a);
+                //Fecha a conexão
+                a.conexao_multicast.interrupt();
+                break;
+            case 2:
+                ProcessoUsuario b = new ProcessoUsuario("Aecio",4444);
+                b.SendOlah("MULTICAST",4444);
+                iniciaConexao(b);
+                break;    
+            case 3:
+                ProcessoUsuario c = new ProcessoUsuario("Dilma",5555);
+                c.SendOlah("MULTICAST",5555);
+                iniciaConexao(c);
+                break;
+        }
+        
+        /*Debug: Digitar manualmente os dados do usuário
         //Lê o nome do usuário
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite um nome de usuário");
@@ -25,12 +54,18 @@ public class Main {
         System.out.println("Digite uma porta para acessar o Sonin Bleinin Torrent");
         int porta = teclado.nextInt();
         
+            
         //Cria um novo processo para o usuário
         ProcessoUsuario a = new ProcessoUsuario(nomeUser,porta);
         
         //Envia um "oi" para a rede, para os outros adicionarem ele
         a.SendOlah("MULTICAST",porta);
-        
+        */
+    }
+    
+    //Inicia as buscas por arquivos
+    private static void iniciaConexao(ProcessoUsuario processo){
+        Scanner teclado = new Scanner(System.in);
         //Começa a rodar a interação com usuário
         while(true){
             String nomeArq;
@@ -39,17 +74,14 @@ public class Main {
             nomeArq = teclado.nextLine();
             if(nomeArq.equals("Q"))
                 break;
-            else if(a.listaDeArquivos.contains(nomeArq)){
+            else if(processo.listaDeArquivos.contains(nomeArq)){
                 System.out.println("VOCÊ JÁ POSSUI ESSE ARQUIVO. TENTE NOVAMENTE");
                 continue;
             }
-            if(a.TemMaisUsuarios())
-                a.PedeArquivo(nomeArq);
+            if(processo.TemMaisUsuarios())
+                processo.PedeArquivo(nomeArq);
             else
                 System.out.println("VOCÊ ESTÁ SOZINHO NESSE ROLÊ.");
         }
-        
-        //Fecha a conexão
-        a.conexao_multicast.interrupt();
     }
 }
